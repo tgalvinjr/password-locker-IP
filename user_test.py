69d044cb836db7b36ef5befd719a27263bd40840
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-13 10:53:48 +0300

    create user test to check the save function

--- user_test.py
+++ user_test.py
@@ -1,26 +1,26 @@
-# import unittest
-# from user import User
+import unittest
+from user import User
 
 
-# class TestUser(unittest.TestCase):
-#     """Define test case for user class behaviour"""
+class TestUser(unittest.TestCase):
+    """Define test case for user class behaviour"""
 
-#     def setUp(self):
-#         """Runs Set Up method before each test case to check if the correct instatiation of the class"""
-#         self.new_user = User("NewUser", "12345")
-
-
-#     def test_init(self):
-#         """Test to ensure that the object is initialized properly"""
-#         self.assertEqual(self.new_user.user_name, "NewUser")
-#         self.assertEqual(self.new_user.password, "12345")
-
-#     def test_save_user(self):
-#         """Method that tests wether an user credentials have been successfully saved"""
-#         self.new_user.save_user()
-#         self.assertEqual(len(User.user_list), 1)
+    def setUp(self):
+        """Runs Set Up method before each test case to check if the correct instatiation of the class"""
+        self.new_user = User("NewUser", "12345")
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_init(self):
+        """Test to ensure that the object is initialized properly"""
+        self.assertEqual(self.new_user.user_name, "NewUser")
+        self.assertEqual(self.new_user.password, "12345")
+
+    def test_save_user(self):
+        """Method that tests wether an user credentials have been successfully saved"""
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
