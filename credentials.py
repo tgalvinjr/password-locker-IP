d4c15364421edb2a2a20f35f8feafa3b488fb236
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-13 11:07:24 +0300

    Create class for credentials

---
+++ credentials.py
@@ -0,0 +1,8 @@
+class Credentials:
+    """Create credentials class"""
+
+    def __init__(self, account_name, account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    credentials_list = []
