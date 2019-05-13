eb7ff0b9069c85b72e2dcc1fa40cc56aca9520ab
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-13 10:50:44 +0300

    create user class

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    """Create class for users"""
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
+    user_list = []
